88d4ed309b02818a88d68b7be1b64c86edbf67dc
shayoni-ui <70857881+shayoni-ui@users.noreply.github.com>
2025-06-03 13:15:37 +0100

    Update test_my_agent.py

--- Test_LLM/agent_backend/tests/test_my_agent.py
+++ Test_LLM/agent_backend/tests/test_my_agent.py
@@ -3,7 +3,7 @@ from src.agents.my_agent import my_agent_function
 from mlflow.types.llm import ChatMessage
 
 def test_agent_basic_response():
-    messages = [ChatMessage(role="user", content="What is the schema of file A?")]
+    messages = [ChatMessage(role="user", content="How many asian patients in the dataset?")]
     response = my_agent_function(messages)
     assert "choices" in response
     assert response["choices"][0]["message"]["content"]
